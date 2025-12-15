fd4d8985417bb96399527ca62a083c04abfc0d71
gangya0327 <king9999427@163.com>
2025-12-15 11:00:24 +0800

    ✨ feat: 鼠标移动轮播图交互

--- duyi/master2021/07-vue/third-vue/src/mock/banner.js
+++ duyi/master2021/07-vue/third-vue/src/mock/banner.js
@@ -7,28 +7,28 @@ Mock.mock("/api/banner", "get", {
     {
       id: 1,
       title: "[韩剧] 灵指",
-      img: "https://nenya.doubanio.com/view/photo/xl/public/p2925518724.jpg?sa_cv=c7920e60f012c3faed064fe1b3729894&sa_ct=693d7923",
+      img: "https://cdn.dribbble.com/userupload/43776512/file/original-be8d415930b5052f45f034915c06945a.jpg?resize=1024x768&vertical=center",
       description:
         "根据同名漫画改编的剧集没有大咖主演，甚至因男主患有过敏性大肠综合症（经常要拉粑粑），被一些网友吐槽是太过恶搞的设定。",
     },
     {
       id: 2,
       title: "[美剧] 雷殛元首",
-      img: "https://nenya.doubanio.com/view/photo/xl/public/p2927010912.jpg?sa_cv=737127b3c8abe05e128b5695a342822b&sa_ct=693d7965",
+      img: "https://cdn.dribbble.com/userupload/15551902/file/original-f1511abd3ef449e40c5aa9835fcf8253.jpg?resize=1024x1024&vertical=center",
       description:
         "作为美国第 20 位总统，加菲尔德的经历很神奇。神职出身的他从未想过参选，起初上台演讲，也是为了替好友拉票。",
     },
     {
       id: 3,
       title: "[美剧] 圣丹尼斯医院 第二季",
-      img: "https://nenya.doubanio.com/view/photo/xl/public/p2927893616.jpg?sa_cv=edb89276ac9b4beb5063700d9a471d80&sa_ct=693d797d",
+      img: "https://cdn.dribbble.com/userupload/44987651/file/3f1b70eaefafbcd664060bf4861caf41.jpg?resize=1024x768&vertical=center",
       description:
         "伪纪录片的形式走进了医护人员的工作里，不仅记录着每个人工作的模样，更借着他们的搞笑生活。",
     },
     {
       id: 4,
       title: "[国产] 三人行",
-      img: "https://nenya.doubanio.com/view/photo/xl/public/p2926823497.jpg?sa_cv=2065670eb3732aae7a65c5bf39971199&sa_ct=693d79c1",
+      img: "https://cdn.dribbble.com/userupload/44423693/file/47766bdef64bab8405296c75e9067e58.jpg?resize=1024x768&vertical=center",
       description: "三个年龄段，不一样的看待世界的视角，不同的人生历练。",
     },
   ],
--- duyi/master2021/07-vue/third-vue/src/views/home/components/CarouselItem.vue
+++ duyi/master2021/07-vue/third-vue/src/views/home/components/CarouselItem.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="carousel-item-container">
-    <div class="carousel-img">
+  <div
+    class="carousel-item-container"
+    ref="container"
+    @mousemove="handleMousemove"
+    @mouseleave="handleMouseleave"
+  >
+    <div class="carousel-img" ref="image" :style="imagePosition">
       <image-loader
         :origin="carousel.img"
         :thumbnail="carousel.img"
@@ -30,12 +35,42 @@ export default {
     return {
       titleWidth: 0,
       descWidth: 0,
+      containerSize: null,
+      innerSize: null,
+      mouseX: 0,
+      mouseY: 0,
     }
+  },
+  computed: {
+    imagePosition() {
+      if (!this.innerSize || !this.containerSize) return {}
+      const extraWidth = this.innerSize.width - this.containerSize.width
+      const extraHeight = this.innerSize.height - this.containerSize.height
+      const left = (-extraWidth / this.containerSize.width) * this.mouseX + "px"
+      const top =
+        (-extraHeight / this.containerSize.height) * this.mouseY + "px"
+
+      return { transform: `translate(${left}, ${top})` }
+    },
+    centerPosition() {
+      return {
+        left: this.containerSize.width / 2,
+        top: this.containerSize.height / 2,
+      }
+    },
   },
   mounted() {
     this.titleWidth = this.$refs.title.offsetWidth
     this.descWidth = this.$refs.desc.offsetWidth
     this.toggleText()
+
+    this.setSize()
+    this.mouseX = this.centerPosition.left
+    this.mouseY = this.centerPosition.top
+    window.addEventListener("resize", this.setSize)
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.setSize)
   },
   watch: {
     activeIndex() {
@@ -65,6 +100,25 @@ export default {
       this.$refs.desc.style.opacity = 1
       this.$refs.desc.style.width = 0
     },
+    setSize() {
+      this.containerSize = {
+        width: this.$refs.container.clientWidth,
+        height: this.$refs.container.clientHeight,
+      }
+      this.innerSize = {
+        width: this.$refs.image.clientWidth,
+        height: this.$refs.image.clientHeight,
+      }
+    },
+    handleMousemove(e) {
+      const rect = this.$refs.container.getBoundingClientRect()
+      this.mouseX = e.clientX - rect.left
+      this.mouseY = e.clientY - rect.top
+    },
+    handleMouseleave() {
+      this.mouseX = this.centerPosition.left
+      this.mouseY = this.centerPosition.top
+    },
   },
 }
 </script>
@@ -75,12 +129,17 @@ export default {
 .carousel-item-container {
   width: 100%;
   height: 100%;
-  background: #ccf;
+  background-color: #fff;
   position: relative;
+  overflow: hidden;
 
   .carousel-img {
-    width: 100%;
-    height: 100%;
+    width: 110%;
+    height: 110%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    // transition: 0.3s;
   }
   .title,
   .desc {
@@ -101,7 +160,6 @@ export default {
     top: calc(50% - 30px);
   }
   .desc {
-    font-size: 1.2em;
     top: calc(50% + 30px);
     color: lighten(@light, 20%);
   }
