64514d73a6e6f4d3597ce3e50c52e3d10bc776b6
gangya0327 <king9999427@163.com>
2026-01-04 17:45:53 +0800

    ✨ feat: 再次实现 zvue-router

--- kkb/web34/vue/vue-demo/src/main.js
+++ kkb/web34/vue/vue-demo/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // import router from './router'
-import router from './yrouter'
+// import router from './yrouter'
+import router from './zrouter'
 
 // import store from './store'
 import store from './ystore'
---
+++ kkb/web34/vue/vue-demo/src/zrouter/zvue-router.js
@@ -0,0 +1,46 @@
+let Vue
+
+class VueRouter {
+  constructor(options) {
+    this.$options = options
+
+    Vue.util.defineReactive(this, 'current', window.location.hash.slice(1) || '/')
+
+    window.addEventListener('hashchange', () => {
+      this.current = window.location.hash.slice(1)
+    })
+  }
+}
+
+VueRouter.install = function (_Vue) {
+  Vue = _Vue
+
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
+    },
+  })
+
+  Vue.component('router-link', {
+    props: { to: { type: String, required: true } },
+    render(h) {
+      return h('a', { attrs: { href: '#' + this.to } }, this.$slots.default)
+    },
+  })
+  Vue.component('router-view', {
+    render(h) {
+      let component = null
+      console.log('this :>> ', this)
+      console.log('this :>> ', this.$router)
+      const route = this.$router.$options.routes.find(
+        (r) => r.path === this.$router.current
+      )
+      if (route) component = route.component
+      return h(component)
+    },
+  })
+}
+
+export default VueRouter
