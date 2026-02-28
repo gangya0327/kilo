4d272d6a81a13ee85aa538b1ca510ff07c15887b
gangya0327 <king9999427@163.com>
2026-02-28 10:19:35 +0800

    🌈 style: 整理目录结构

--- kkb/web34/react/my-react-app/src/App.js
+++ kkb/web34/react/my-react-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import ClassComponent from './pages/ClassComponent';
-import FunctionComponent from './pages/FunctionComponent';
-import LifeCycle from './pages/LifeCycle';
+import ClassComponent from './pages/1-component/ClassComponent';
+import FunctionComponent from './pages/1-component/FunctionComponent';
+import LifeCycle from './pages/2-lifecycle/LifeCycle';
 
 function App() {
   return (
